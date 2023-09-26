55b857bb76215cb12fddadc821dc249aadb91132
falabricio <144167385+falabricio@users.noreply.github.com>
2023-09-26 15:32:45 -0300

    v1

---
+++ f.py
@@ -0,0 +1,10 @@
+n1 = int(input('primeiro número'))
+n2 = int(input('segundo número'))
+n3 = int(input('terceiro número'))
+n4 = int(input('quarto número'))
+n5 = int(input('quinto número'))
+# aqui realiza o calculo da soma
+res = n1+n2+n3+n4+n5
+res = res/5
+
+print('a média entre as notas {}, {}, {}, {} e {} é {}'.format(n1,n2,n3,n4,n5,res))
